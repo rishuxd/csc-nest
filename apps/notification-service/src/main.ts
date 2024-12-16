c1be452354d2fc8cd09c1474d4f2a96570dd5b1d
Jyotiswaroop Srivastava <srivastavjyotiswaroop@gmail.com>
2024-12-16 19:26:45 +0530

    fix: grpc added

--- apps/notification-service/src/main.ts
+++ apps/notification-service/src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { NotificationServiceModule } from './notification-service.module';
+import { Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(NotificationServiceModule);
-  await app.listen(process.env.port ?? 3000);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,OPTIONS',
+    credentials: true,
+  });
+
+  app.connectMicroservice({
+    transport: Transport.GRPC,
+    options: {
+      url: 'localhost:5051',
+      package: ['notify'],
+      protoPath: [join(__dirname, '../notify.proto')],
+    },
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(5002);
 }
 bootstrap();
