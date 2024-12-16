4f4ca871a20ef6c61e1111dc2c6734b2f2a4304d
Jyotiswaroop Srivastava <srivastavjyotiswaroop@gmail.com>
2024-12-16 19:06:35 +0530

    fix: error fix

--- apps/notification-service/src/notify/notify.module.ts
+++ apps/notification-service/src/notify/notify.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { NotifyService } from './notify.service';
 import { NotifyController } from './notify.controller';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { NotificationSchema } from './schema/notification.schema';
+import { SseModule } from '../sse/sse.module';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    SseModule,
+    MongooseModule.forRoot(process.env.DB_URI),
+    MongooseModule.forFeature([
+      { name: 'Notification', schema: NotificationSchema },
+    ]),
+  ],
   controllers: [NotifyController],
   providers: [NotifyService],
 })
